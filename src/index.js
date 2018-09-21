73f33c4c13e1fa9e936caf7e07a15562e7328163
Aleksandr2102 <aleksschpak@gmail.com>
2018-09-21 09:08:43 +0300

    'sorter'

--- src/index.js
+++ src/index.js
@@ -1,30 +1,53 @@
 class Sorter {
   constructor() {
     // your implementation
+      this.arr = [];
+      this.function = function(a, b) {
+        return a - b;
+      }
   }
 
   add(element) {
     // your implementation
+    this.arr.push(element);
   }
 
   at(index) {
     // your implementation
+    return this.arr[index];
+
   }
 
   get length() {
     // your implementation
+    return this.arr.length;
   }
 
   toArray() {
     // your implementation
+    return this.arr;
   }
 
   sort(indices) {
     // your implementation
+    var endArr = [];
+    indices.sort(function(a, b) { return a - b;});
+
+    for(var i = 0; i < indices.length; i++) {
+      endArr.push(this.arr[indices[i]]);
+    }
+
+      endArr.sort(this.function);
+      for (var i = 0; i < indices.length; i++) {
+          this.arr[indices[i]] = endArr[i];
+      }
+    
+ 
   }
 
   setComparator(compareFunction) {
     // your implementation
+   this.function = compareFunction;
   }
 }
 
